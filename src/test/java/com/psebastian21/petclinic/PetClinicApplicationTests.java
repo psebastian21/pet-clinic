abb77d80c3fe53a4e301057bf421a0d6519f149f
Paulo Garcia <psebastian21@gmail.com>
2022-06-10 20:27:45 -0500

    first commit

---
+++ src/test/java/com/psebastian21/petclinic/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psebastian21.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
